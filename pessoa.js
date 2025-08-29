2999cc75a1c88b8916623629a39ae075721047b5
unknown <adailton.santos17@etec.sp.gov.br>
2025-08-28 21:16:20 -0300

    Realização do exercício proposto

--- pessoa.js
+++ pessoa.js
@@ -1,13 +1,29 @@
-const pessoa = {
-    nome: "Adailton",
-    idade: 27,
-    imprimirNome: function(){
-        return `${this.nome} tem ${this.idade} anos`;
-    },
+// const pessoa = {
+//     nome: "Adailton",
+//     idade: 27,
+//     imprimirNome: function(){
+//         return `${this.nome} tem ${this.idade} anos`;
+//     },
 
-    beber: function(){
-        return false
+//     beber: function(){
+//         return false
+//     }
+// }
+
+// console.log(pessoa, pessoa.imprimirNome(), pessoa.beber())
+
+/*
+Exercício:
+Modele uma classe/objeto de um produto de uma papelaria esse produto precisa ter como atributos: nome e preço e um método que calcule o valor de uma venda quando passados uma quantidade como parâmetro;
+*/
+
+const produto = {
+    nome: "caneta",
+    preço: 2,
+    cor: "azul",
+    calcular: function(qtd){
+        return this.preço * qtd
     }
 }
 
-console.log(pessoa, pessoa.imprimirNome(), pessoa.beber())
+console.log(produto.calcular(3), produto.cor)
